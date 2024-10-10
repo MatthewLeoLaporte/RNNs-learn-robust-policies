44f0ea9fc78ad06a803359350d8ad8509a7b357a
Matt Laporte <matt.l.laporte@gmail.com>
2024-10-09 20:24:20 -0400

    Evaluate trained models on curl tasks, and plot single trials
    - Make two variants of the control validation task, one of which has multiple
      center out sets of 32 directions (for the main analyses) and one with a single
      set of 7 (for visualization).
    - Load prior checkpoints in notebook 1-2a in cases where model parameters become
      unstable by the end of training runs.
    - Add a range of constant-magnitude curl field reaching tasks for model evaluation.
    - Draft single-condition plots for comparison of curl field responses of models
      trained under different perturbation scales.

---
+++ src/rnns_learn_robust_motor_policies/tree_utils.py
@@ -0,0 +1,11 @@
+from collections.abc import Callable
+import equinox as eqx 
+from jaxtyping import PyTree
+
+
+def swap_model_trainables(model: PyTree[..., "T"], trained: PyTree[..., "T"], where_train: Callable):
+    return eqx.tree_at(
+        where_train,
+        model,
+        where_train(trained),
+    )
