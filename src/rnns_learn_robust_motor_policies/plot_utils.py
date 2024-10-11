80c0e03c00745cf0e0e9978320a8100a45fb12a3
Matt Laporte <matt.l.laporte@gmail.com>
2024-10-11 16:23:22 -0400

    Simplify flow of notebook 1-2a
    The "control" model is now treated as a 0 curl variant,
    and is evaluated along with all the other curl variants.
    
    Save figures to subdirectories for better organization.

--- src/rnns_learn_robust_motor_policies/plot_utils.py
+++ src/rnns_learn_robust_motor_policies/plot_utils.py
@@ -1,29 +1,37 @@
 
+from pathlib import Path
+from typing import Optional
 import matplotlib.figure as mplfig
 import plotly.graph_objects as go
 
 
-def get_savefig_func(fig_dir, suffix=""):
+def get_savefig_func(fig_dir: Path, suffix=""):
     """Returns a function that saves Matplotlib and Plotly figures to file in a given directory.
     
     This is convenient in notebooks, where all figures made within a single notebook are generally
     saved to the same directory. 
     """
     
-    def savefig(fig, label, ext='.svg', transparent=True, **kwargs): 
-        
+    def savefig(fig, label, ext='.svg', transparent=True, subdir: Optional[str] = None, **kwargs): 
+
+        if subdir is not None:
+            save_dir = fig_dir / subdir
+            save_dir.mkdir(exist_ok=True, parents=True) 
+        else:
+            save_dir = fig_dir           
+
         label += suffix
         
         if isinstance(fig, mplfig.Figure):
             fig.savefig(
-                fig_dir / f"{label}{ext}",
+                save_dir / f"{label}{ext}",
                 transparent=transparent, 
                 **kwargs, 
             )
         
         elif isinstance(fig, go.Figure):
             # Save HTML for easy viewing, and JSON for embedding.
-            fig.write_html(fig_dir / f'{label}.html')
-            fig.write_json(fig_dir / f'{label}.json')
+            fig.write_html(save_dir / f'{label}.html')
+            fig.write_json(save_dir / f'{label}.json')
     
     return savefig
